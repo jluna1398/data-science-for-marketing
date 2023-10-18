051a1d469d2f4a79a3c595c79b2c0e18778b1294
jluna1398 <60058404+jluna1398@users.noreply.github.com>
2023-10-17 18:40:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,15 @@ import numpy as np
 
 
 
+
 st.title("Heelo")
+sns.set_theme(style="whitegrid")
+
+# Make an example dataset with y ~ x
+rs = np.random.RandomState(7)
+x = rs.normal(2, 1, 75)
+y = 2 + 1.5 * x + rs.normal(0, 2, 75)
+
+# Plot the residuals after fitting a linear model
+fig  = sns.residplot(x=x, y=y, lowess=True, color="g")
+st.pyplot(fig)
